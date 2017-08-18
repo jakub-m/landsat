b007d6d85be485ebebe64d46ed1d26f5bc24342f
Jakub Mikians <jakub.mikians@gmail.com>
2017-08-18 22:28:27 +0200

    List assets from CLI

--- main/landsat.go
+++ main/landsat.go
@@ -1,54 +1,74 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"jakub-m/landsat/http"
 	"jakub-m/landsat/nasaapi/assets"
-	"jakub-m/landsat/nasaapi/imagery"
+	"jakub-m/landsat/nasaapi/types"
 	"log"
 	"time"
 )
 
 const (
-	apiKey = "2YzaD3F2NoQusPVLco3ETnhbAR9ehVngscQfgUsZ"
+	dateFormat = "2006-01-02"
+	timeFormat = "2006-01-02T15:04:05"
 )
 
-func main() {
-	req := &assets.Request{
-		Lat:    54.401325,
-		Lon:    18.572122,
-		Begin:  time.Date(2014, time.August, 1, 0, 0, 0, 0, time.UTC),
-		End:    time.Date(2016, time.August, 1, 0, 0, 0, 0, time.UTC),
-		APIKey: apiKey,
-	}
-	res, err := assets.Get(req)
-	if err != nil {
-		panic(err)
-	}
-	for i, r := range res.Results {
-		log.Println(r.Date)
-		err := fetchForDate(req.Lat, req.Lon, r.Date, i)
-		if err != nil {
-			panic(err)
-		}
-	}
-	// scanChan := landsat.GenerateScanChan()
-	// landsat.ProcessScan(scanChan)
+var args struct {
+	apiKey string
+	begin  date
+	end    date
+	lat    float64
+	lon    float64
 }
 
-func fetchForDate(lat, lon float32, date time.Time, i int) error {
-	req := &imagery.Request{
-		Lat:    lat,
-		Lon:    lon,
-		Date:   date,
-		APIKey: apiKey,
-	}
-	res, err := imagery.Get(req)
+type date struct {
+	value time.Time
+}
+
+func (d *date) Set(value string) error {
+	time, err := time.Parse(dateFormat, value)
 	if err != nil {
 		return err
 	}
-	fileName := fmt.Sprintf("image_%d.png", i+1)
-	log.Println(date, fileName)
-	http.FetchImage(res.URL, fileName)
+	d.value = time
 	return nil
+}
+
+func (d *date) String() string {
+	return d.value.Format(dateFormat)
+}
+
+func init() {
+	flag.StringVar(&args.apiKey, "api-key", "", "API key")
+	flag.Float64Var(&args.lat, "lat", 0, "Latitude")
+	flag.Float64Var(&args.lon, "lon", 0, "Longitude")
+	flag.Var(&args.begin, "begin", "Begin date")
+	flag.Var(&args.end, "end", "End date")
+	flag.Parse()
+}
+
+func main() {
+	req := &assets.Request{
+		Lat:    float32(args.lat),
+		Lon:    float32(args.lon),
+		Begin:  args.begin.value,
+		End:    args.end.value,
+		APIKey: types.APIKey(args.apiKey),
+	}
+
+	res, err := assets.Get(req)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println("API key:", args.apiKey)
+	log.Println("Latitude:", args.lat)
+	log.Println("Longitude:", args.lon)
+	log.Println("Begin date:", args.begin.String())
+	log.Println("End date:", args.end.String())
+	log.Printf("Got %d assets: ", res.Count)
+	for _, r := range res.Results {
+		fmt.Printf("%s\t%g\t%g\t%s\n", r.ID, req.Lat, req.Lon, r.Date.Format(timeFormat))
+	}
 }
